dd8de665d813a7daa865d073a1410bcf25772bff
martinhuerta01 <martin.huerta@logictracker.com>
2026-04-29 20:05:41 -0300

    Update auth.py

--- models/auth.py
+++ models/auth.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class LoginRequest(BaseModel):
     usuario: str
@@ -8,3 +9,5 @@ class TokenResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
     usuario: str
+    rol: str
+    modulos: Optional[list[str]] = None
